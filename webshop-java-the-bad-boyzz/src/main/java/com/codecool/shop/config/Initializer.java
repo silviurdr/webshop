d1ca9ba2fae9c29519da48dd14450d5e59f7265f
alecsanderu <oanceaalexander@yahoo.com>
2020-06-22 16:29:18 +0300

    tests

--- webshop-java-the-bad-boyzz/src/main/java/com/codecool/shop/config/Initializer.java
+++ webshop-java-the-bad-boyzz/src/main/java/com/codecool/shop/config/Initializer.java
@@ -31,11 +31,14 @@ public class Initializer implements ServletContextListener {
 
         //setting up a new product category
         ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
+        ProductCategory notebook = new ProductCategory("Notebook", "Hardware", "A notebook computer is a battery- or AC-powered personal computer generally smaller than a briefcase that can easily be transported and conveniently used in temporary spaces such as on airplanes, in libraries, temporary offices, and at meetings..");
         productCategoryDataStore.add(tablet);
+        productCategoryDataStore.add(notebook);
 
         //setting up products and printing it
         productDataStore.add(new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
         productDataStore.add(new Product("Lenovo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
         productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
+        productDataStore.add(new Product("HP Pavilion", 700, "USD", "HP Laptop. Great value at a greater price", notebook, amazon));
     }
 }
