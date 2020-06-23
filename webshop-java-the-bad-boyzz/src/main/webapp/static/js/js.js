0cf54bb6177666ad8a04eb83bff03331b2d24a46
alecsanderu <oanceaalexander@yahoo.com>
2020-06-23 20:28:20 +0300

    checkout working

--- webshop-java-the-bad-boyzz/src/main/webapp/static/js/js.js
+++ webshop-java-the-bad-boyzz/src/main/webapp/static/js/js.js
@@ -5,3 +5,8 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+// let redeembutton = document.querySelector(".redemcode");
+// redeembutton.addEventListener("click", function (e) {
+//     e.preventDefault();
+// })
