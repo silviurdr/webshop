a6d90f35497875c506b2aa3bf46ca384d99dd3cf
MihaiBarbu93 <mihai.barbu1993@gmail.com>
2020-06-25 11:57:03 +0300

    Adding info to json file

--- webshop-java-the-bad-boyzz/src/main/java/com/codecool/shop/controller/PaymentConfController.java
+++ webshop-java-the-bad-boyzz/src/main/java/com/codecool/shop/controller/PaymentConfController.java
@@ -6,6 +6,7 @@ import com.codecool.shop.dao.CartProductDao;
 import com.codecool.shop.dao.implementation.BillingInformationDaoMem;
 import com.codecool.shop.dao.implementation.CartProductDaoMem;
 import com.codecool.shop.dao.implementation.CheckoutDaoMem;
+import com.codecool.shop.model.Order;
 import com.codecool.shop.model.Product;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
@@ -25,6 +26,8 @@ import org.json.simple.JSONObject;
 
 @WebServlet(urlPatterns = {"/confirmation"})
 public class PaymentConfController extends HttpServlet {
+
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         BillingInformationDao billingInfo= BillingInformationDaoMem.getInstance();
@@ -36,12 +39,15 @@ public class PaymentConfController extends HttpServlet {
         for (Product p : cartProducts.getAll()){
             sum+=p.getDefaultPrice();
         }
+        
+        Order orderInfo = billingInfo.getFirst();
 
+        orderInfo.Jsonify();
         String sum2  = String.format("%.1f", sum);
         context.setVariable("total",sum2);
         context.setVariable("products", cartProducts.getAll() );
         context.setVariable("productsSet",prodForCart);
-        context.setVariable("billingInfo",billingInfo.getFirst());
+        context.setVariable("billingInfo",orderInfo);
         engine.process("product/confirmation.html", context, resp.getWriter());
 
     }
