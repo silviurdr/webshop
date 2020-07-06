8e3647de6138255be4d8b3a15b8f0db887130463
silviurdr <silviu.catrina@gmail.com>
2020-07-06 20:04:22 +0300

    Add User model

---
+++ webshop-java-the-bad-boyzz/src/main/java/com/codecool/shop/model/User.java
@@ -0,0 +1,72 @@
+package com.codecool.shop.model;
+
+public class User {
+
+    private String name;
+
+    private int userID = 1;
+    private String email;
+    private String mobileNumber;
+    private String password;
+    private String status;
+
+    public User(String name, String email, String mobileNumber, String password, String status) {
+        this.userID++;
+        this.name = name;
+        this.email = email;
+        this.mobileNumber = mobileNumber;
+        this.password = password;
+        this.status = status;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return userID;
+    }
+
+    public void setId(int id) {
+        this.userID = id;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+}
