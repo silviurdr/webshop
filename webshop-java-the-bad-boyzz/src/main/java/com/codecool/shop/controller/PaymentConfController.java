813a55903353dd8ba08d0f5caecceee362b1fe7d
silviurdr <silviu.catrina@gmail.com>
2020-07-09 12:27:53 +0300

    Add consistent nav on more pages

--- webshop-java-the-bad-boyzz/src/main/java/com/codecool/shop/controller/PaymentConfController.java
+++ webshop-java-the-bad-boyzz/src/main/java/com/codecool/shop/controller/PaymentConfController.java
@@ -15,6 +15,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(urlPatterns = {"/confirmation"})
@@ -25,12 +26,20 @@ public class PaymentConfController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         BillingInformationDao billingInfo= BillingInformationDaoMem.getInstance();
         CartProductDao cartProducts = CartProductDaoMem.getInstance();
+        CartProductDao cartProductCategoryDataStore = CartProductDaoMem.getInstance();
         WebContext context = new WebContext(req, resp, req.getServletContext());
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
 
+        HttpSession session = req.getSession();
+
         float sum = 0;
+        int noOfProducts = 0;
         for (Product p : cartProducts.getAll().keySet()){
             sum+=p.getDefaultPrice();
+        }
+
+        for (Product p : cartProductCategoryDataStore.getAll().keySet()) {
+            noOfProducts += cartProductCategoryDataStore.getAll().get(p);
         }
         
         Order orderInfo = billingInfo.getFirst();
@@ -41,6 +50,9 @@ public class PaymentConfController extends HttpServlet {
         context.setVariable("products", cartProducts.getAll() );
         context.setVariable("productsSet",cartProducts.getAll());
         context.setVariable("billingInfo",orderInfo);
+        context.setVariable("noOfProducts", 0);
+        context.setVariable("userSession", session.getAttribute("userSession") != null ? session.getAttribute("userSession")  : "No");
+
         cartProducts.clearCart();
         engine.process("product/confirmation.html", context, resp.getWriter());
 
